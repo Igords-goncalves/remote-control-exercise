4f9a860a4c57188b9993bf0c5c78290d635b0939
Igor Gonçalves <igordossantos@gmail.com>
2021-11-23 16:26:51 -0300

    Construindo a interface

---
+++ aula_6.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            height: 50vh;
+            background-color: black;
+            color: white;
+            font-size: 1rem;
+        }
+    </style>
+    <title>Aula 06</title>
+</head>
+<body>
+    <pre>
+    <?php 
+       
+    ?>
+    </pre>
+</body>
+</html>
